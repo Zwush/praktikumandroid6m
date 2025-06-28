eebb472e732a7f986bf0f2aa17bc62e946831599
Maulana0012 <waytrial03@gmail.com>
2025-06-28 10:03:44 +0800

    pertemuan_3

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,19 @@
 import 'package:flutter/material.dart';
+import 'agenda_list.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            'AHMAD MAULANA MALIK IBRAHIM',
-            style: TextStyle(color: Colors.white),
-          ),
-          backgroundColor: Colors.blue,
-        ),
-        body: Center(child: Text('MY FIRST APPLICATION')),
-      ),
+      title: 'Agenda App',
+      theme: ThemeData(primarySwatch: Colors.teal),
+      home: const AgendaList(),
     );
   }
 }
